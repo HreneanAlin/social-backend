96d7d35820466d8735844cee5a87811a48b548cb
alinh-sp <alin.hrenean@sightplan.com>
2021-07-11 16:37:36 +0300

    postgress migration

--- core/settings.py
+++ core/settings.py
@@ -87,9 +87,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'faceRequired',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '2much2hate',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
