1e5eb691304bfe4179e6170b6c8b2aa821bb24df
alinh-sp <alin.hrenean@sightplan.com>
2021-07-29 21:16:27 +0300

    accept friend request

--- friend_requests/mutations.py
+++ friend_requests/mutations.py
@@ -3,6 +3,7 @@ from users.models import ExtendUser
 from graphene_subscriptions.events import SubscriptionEvent
 from .models import FriendRequest
 from .events import NEW_FRIEND_REQUEST
+from .enums import ACCEPTED, PENDING, DECLINED
 
 
 class SendFriendRequest(graphene.Mutation):
@@ -20,14 +21,38 @@ class SendFriendRequest(graphene.Mutation):
             friend_request = FriendRequest(
                 user_from=current_user, user_to=user_to_send)
             friend_request.save()
-            print("id1 ",friend_request.user_to.username)
-            event = SubscriptionEvent(operation=NEW_FRIEND_REQUEST,instance=friend_request)
+            print("id1 ", friend_request.user_to.username)
+            event = SubscriptionEvent(
+                operation=NEW_FRIEND_REQUEST, instance=friend_request)
             event.send()
             return SendFriendRequest(success=True)
         except:
             return SendFriendRequest(success=False)
 
 
+class AcceptFriendRequest(graphene.Mutation):
+
+    class Arguments:
+        friend_request_id = graphene.Int()
+
+    success = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, friend_request_id):
+        try:
+            current_user = info.context.user
+            friend_request = FriendRequest.objects.filter(
+                user_to=current_user).filter(status=PENDING).get(id=friend_request_id)
+            friend_request.status = ACCEPTED
+            friend_request.save()
+            current_user.friends.add(friend_request.user_from)
+            current_user.save()
+            print(friend_request)
+            return AcceptFriendRequest(success=True)
+        except:
+            return SendFriendRequest(success=False)
+
 
 class FriendRequestMutation(graphene.ObjectType):
     send_friend_request = SendFriendRequest.Field()
+    accept_friend_request = AcceptFriendRequest.Field()
--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class ExtendUser(AbstractUser):
     country = models.CharField(max_length=255,verbose_name='country',blank=False)
     profile_picture = models.ImageField(upload_to=upload_path)
     my_test = models.CharField(max_length=255,verbose_name='my_test',blank=True)
+    friends = models.ManyToManyField("ExtendUser",blank=True)
 
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
