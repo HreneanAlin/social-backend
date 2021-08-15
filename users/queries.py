255fac142996cfaa64cb8f3006ec209ad0438700
alinh-sp <alin.hrenean@sightplan.com>
2021-08-15 10:55:22 +0300

    staff req only

--- users/queries.py
+++ users/queries.py
@@ -1,18 +1,21 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import ExtendUser
+from graphql_jwt.decorators import staff_member_required
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = ExtendUser
-        exclude = ("password","email")
+        exclude = ("password", "email")
 
 
 class UsersQuery(graphene.ObjectType):
     users_by_query = graphene.List(
         UserType, query=graphene.String())
     user_by_username = graphene.Field(UserType, username=graphene.String())
+
+    get_all_users = graphene.List(UserType)
 
     def resolve_users_by_query(root, info, query):
         current_user = info.context.user
@@ -34,3 +37,7 @@ class UsersQuery(graphene.ObjectType):
         if(not current_user):
             return None
         return current_user
+
+    @staff_member_required
+    def resolve_get_all_users(root, info):
+        return ExtendUser.objects.all()
